b164d73a7d105a3e29791ce8643047e1acf47d98
FaisalAhamedShaik <faisaleric5530@gmail.com>
2025-09-18 10:27:28 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5123/api'
-  // apiUrl: 'https://fletnix-apis-iiot.onrender.com/api'
+  // apiUrl: 'http://localhost:5123/api'
+  apiUrl: 'https://fletnix-apis-iiot.onrender.com/api'
 };
